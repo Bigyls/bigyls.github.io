2c9603f51cc4a0a4450f467ecb580d398324e178
Bigyls <74175999+Bigyls@users.noreply.github.com>
2025-06-13 14:50:53 +0200

    feat: implement csp policies

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 // imports
-import {links} from "./links.js";
+import { links } from "./links.js";
 
 //constants
 const linkContainer = document.getElementById("links");
 
 //functions
-function addLink(name, link) {
-  return `
-  <a href="${link}" class="link" target="blank">
-    <span>${name}  </span>
-    <img class="linkIcon" src="./assets/link-solid.svg" alt=""/>
-  </a>
-  `;
+function createLinkElement(name, link) {
+  const a = document.createElement("a");
+  a.href = link;
+  a.className = "link";
+  a.target = "blank";
+  a.rel = "noopener noreferrer";
+
+  const span = document.createElement("span");
+  span.textContent = name + "  ";
+
+  const img = document.createElement("img");
+  img.className = "linkIcon";
+  img.src = "./assets/link-solid.svg";
+  img.alt = "";
+
+  a.appendChild(span);
+  a.appendChild(img);
+
+  return a;
 }
 
 // logic
-let allLinks = "";
-
 links.forEach((ele) => {
   let link = ele.link;
   let name = ele.name;
 
-  allLinks += addLink(name, link);
+  const linkElement = createLinkElement(name, link);
+  linkContainer.appendChild(linkElement);
 });
-
-linkContainer.innerHTML = allLinks;
